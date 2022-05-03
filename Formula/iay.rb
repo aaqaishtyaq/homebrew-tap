933d8ad06aa8da7b2a82d5f4aa702c9939a88721
aaqaishtyaq <aaqaishtyaq@users.noreply.github.com>
2022-05-03 11:09:05 +0000

    Bump iay 0.2.0

--- Formula/iay.rb
+++ Formula/iay.rb
@@ -1,11 +1,11 @@
 class Iay < Formula
   desc "Minimilist shell prompt."
   homepage "https://github.com/aaqaishtyaq/iay"
-  version "0.1.8"
+  version "0.2.0"
 
   if OS.mac?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.1.8/iay-x86_64-apple-darwin.tar.gz"
-    sha256 "f48aee7225fd27a60c344d5e6fd531c56edd115b7c3d8c1f70532a56559602c0"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.2.0/iay-x86_64-apple-darwin.tar.gz"
+    sha256 "40805149ddf7234615e3140bd083256bd685a67d29823427f0aece7c5bfc7420"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,13 +20,13 @@ class Iay < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.1.8/iay-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "491235db9665b86662be36cf95e42ffee4ec746f4ddc89bfff0fadc35af20e9a"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.2.0/iay-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "043bf46ba4e62d67c148cc6e7973a204ecf78b3c7ea810c0ae4517d7f7a0e179"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.1.8/iay-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "3b4466e68b985b4b78f441dc507c86bd8f3480ed612e7b7f6e4c90b9844066af"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.2.0/iay-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "53b1ba44557f161999b0716e76db56620b5de8a38f52fe18c68cbe0f6a9c8272"
   end
 
   conflicts_with "iay"
