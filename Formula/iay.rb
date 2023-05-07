aa916813cdee6ce1849e1fd34379ccd269dafa9a
aaqaishtyaq <aaqaishtyaq@users.noreply.github.com>
2023-05-07 11:27:44 +0000

    Bump iay 0.4.2

--- Formula/iay.rb
+++ Formula/iay.rb
@@ -1,32 +1,26 @@
 class Iay < Formula
   desc "Minimilist shell prompt."
   homepage "https://github.com/aaqaishtyaq/iay"
-  version "0.4.0"
+  version "0.4.2"
 
-  if OS.mac?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.0/iay-x86_64-apple-darwin.tar.gz"
-    sha256 "54389247b6a864a6ea2547ca4a06051c68713fd0b490ecde87b8ac1ad7f6a070"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-x86_64-apple-darwin.tar.gz"
+    sha256 "49d69bbb20813cd12a1e62aec6c636d653abbb9eb39e562db52965eb342ca3ba"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    def caveats
-      <<~EOS
-        The darwin_arm64 architecture is not supported for this product
-        at this time, however we do plan to support this in the future. The
-        darwin_amd64 binary has been installed and may work in
-        compatibility mode, but it is not fully supported.
-      EOS
-    end
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-aarch64-apple-darwin.tar.gz"
+    sha256 "4b653385b9ba2dd730f5f4c5b4b627dd3cc3fdb35482ab5df12c753bc5bf9b8a"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.0/iay-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "43f6650aff830ae8b12ae4d38c0efbd1092b53aee2a3a6878f2bcc51d1e89769"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "997e77e2414d94316eca54ca74e581507deeb2c79e2f6c3cffbf0c8f0f8f787f"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.0/iay-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "8a7e08235a2cd7be3c19c974209a9f8c7529c85b1499e6f8c04bd924325bded7"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "01f23fc91f5532230f399f1bb2c70d60196a235c20a6c5edb7abd2e18fde665c"
   end
 
   conflicts_with "iay"
