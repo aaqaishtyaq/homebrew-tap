09881d237035a2d40787b8419609dd6d54f6dc0a
aaqaishtyaq <aaqaishtyaq@users.noreply.github.com>
2025-01-02 12:07:59 +0000

    Bump iay 0.4.3

--- Formula/iay.rb
+++ Formula/iay.rb
@@ -1,26 +1,26 @@
 class Iay < Formula
   desc "Minimilist shell prompt."
   homepage "https://github.com/aaqaishtyaq/iay"
-  version "0.4.2"
+  version "0.4.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-x86_64-apple-darwin.tar.gz"
-    sha256 "49d69bbb20813cd12a1e62aec6c636d653abbb9eb39e562db52965eb342ca3ba"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.3/iay-x86_64-apple-darwin.tar.gz"
+    sha256 "6e34baef53187be97f7921cbef4bd6b5b2ac8a966f682e9de2cede4e45e414dd"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-aarch64-apple-darwin.tar.gz"
-    sha256 "4b653385b9ba2dd730f5f4c5b4b627dd3cc3fdb35482ab5df12c753bc5bf9b8a"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.3/iay-aarch64-apple-darwin.tar.gz"
+    sha256 "72068b6a8730feb2c3233c6c7115153d92b4523d9930839f83cb05f74ed2d1cc"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "997e77e2414d94316eca54ca74e581507deeb2c79e2f6c3cffbf0c8f0f8f787f"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.3/iay-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "3a126a3ec19a80dd890273439c6f765bcfc63d960e21c5a582b0ac366bfac2f0"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.2/iay-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "01f23fc91f5532230f399f1bb2c70d60196a235c20a6c5edb7abd2e18fde665c"
+    url "https://github.com/aaqaishtyaq/iay/releases/download/v0.4.3/iay-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "3d0076b9a65e7644f18dc79166adc92350c867fbbd2687969a34e4edfd2db60b"
   end
 
   conflicts_with "iay"
